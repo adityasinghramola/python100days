00152acfab45ae0677e5602a9758b617133149ac
AdityaR <108941624+adityasinghramola@users.noreply.github.com>
2025-04-14 17:05:00 +0530

    Add files via upload

---
+++ task.py
@@ -0,0 +1,4 @@
+print("Welcome to My First Project.")
+city = input("Which City Did you Grew Up In?\n ")
+pet = input("what is the name of your pet?\n ")
+print("your band name could be " + city + " " + pet)
